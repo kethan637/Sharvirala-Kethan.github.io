c18d688b992c0ff78ded17f4d304912372b86878
Sharvirala.Kethan <Sharvirala.Kethan@mu-sigma.com>
2025-02-18 20:34:10 +0530

    added Skills and experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Trainee Decision Scientist II',
+    company: "Musigma",
+    duration: "(Jan 2025 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Trainee Decision Scientist I",
+    company: "Musigma",
+    duration: "(Jun 2023 - Dec 2024)"
   },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
 ]
