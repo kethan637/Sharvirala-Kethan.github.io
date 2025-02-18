8ab89fa2aaebdf24f8e6aaaf03c65469c1741eed
Sharvirala.Kethan <Sharvirala.Kethan@mu-sigma.com>
2025-02-18 21:10:08 +0530

    Added projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,19 +1,40 @@
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
+        name: 'Calorie Burnt Prediction Using ML Algorithms.',
+        description: "The Calorie Burn Prediction app estimates the calories you burn based on factors like age, weight, height, gender, heart rate, and workout duration. Using machine learning, it provides real-time predictions to help track fitness goals and optimize workouts.",
+        tools: [
+            "Python",
+            "Scikit-Learn",
+            "TensorFlow/Keras",
+            "XGBoost",
+            "Pandas",
+            "NumPy",
+            "Matplotlib",
+            "Seaborn"
+          ],
         role: 'Backend Developer',
         code: '',
         demo: '',
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
+        name: 'Reviews using Sentimental Anyalsis',
+        description: 'In this project we mainly focus on the sentimental Analysis.By using sentimental Analysis we can able to find the nature of reviews like postive or negative by using this method we will find the products need any changes what and all need to do in prodict roaster.',
+        tools: [
+            "Python",
+            "NLTK",
+            "TextBlob",
+            "VADER",
+            "Scikit-Learn",
+            "TensorFlow/Keras",
+            "Transformers (BERT, RoBERTa)",
+            "Pandas",
+            "NumPy",
+            "Matplotlib",
+            "Seaborn",
+          ],
+        role: 'Backend Developer',
         code: '',
         demo: '',
     },
@@ -33,7 +54,7 @@ export const projectsData = [
         tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
         code: '',
         demo: '',
-        role: 'Full Stack Developer',
+        role: 'Developer',
     }
 ];
 
