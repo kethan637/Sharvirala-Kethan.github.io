6f57d9a6dda1f9aa5d02cc1698b58e060446e303
kethan637 <Kethankumar637@gmail.com>
2025-12-31 15:51:44 +0530

    Title font decrease

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -8,22 +8,25 @@ function Navbar() {
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
           <Link
-  href="/"
-  className="
-    text-[#16f2b3]
-    font-bold
-    leading-tight
-    text-xl
-    sm:text-2xl
-    md:text-3xl
-    break-words
-  "
->
-  Unfolding My Career :{" "}
-  <span className="whitespace-nowrap">
-    Sharvirala Kethan
-  </span>
-</Link>
+            href="/"
+            className="
+              text-[#16f2b3]
+              font-bold
+              leading-tight
+              text-base
+              sm:text-lg
+              md:text-xl
+              lg:text-2xl
+              xl:text-3xl
+              break-words
+            "
+          >
+            Unfolding My Career :{" "}
+            <span className="whitespace-nowrap">
+              Sharvirala Kethan
+            </span>
+          </Link>
+
         </div>
 
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
