c0e1ee17ea710d98d036b3f16e94290f1ec7c872
kethan637 <Kethankumar637@gmail.com>
2026-02-01 12:22:34 +0530

    Updated Expereince

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Trainee Decision Scientist II',
+    title: 'Trainee Decision Scientist III',
     company: "Musigma",
-    duration: "(Jan 2025 - Present)"
+    duration: "(Jan 2026 - Present)"
   },
   {
     id: 2,
+    title: 'Trainee Decision Scientist II',
+    company: "Musigma",
+    duration: "(Jan 2025 - Dec 2025)"
+  },
+  {
+    id: 3,
     title: "Trainee Decision Scientist I",
     company: "Musigma",
     duration: "(Jun 2023 - Dec 2024)"
